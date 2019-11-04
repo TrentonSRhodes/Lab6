3a6659b11b77e1515f288117ee8bee49ab13260c
trent <trent@DESKTOP-NKK8L7L>
2019-11-03 21:09:32 -0600

    Merge Trenton/master
    Conflicts:
    	src/main/java/salesTax.java

---
+++ src/main/java/monthClassMain/months.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monthClassMain;
+
+/**
+ *
+ * @author trent
+ */
+public class months {
+    private int monthsNumber;
+
+    public int getMonthsNumber() {
+        return monthsNumber;
+    }
+
+    public void setMonthsNumber(int monthsNumber) {
+        this.monthsNumber = monthsNumber;
+    }
+
+    public months(int monthsNumber) {
+        this.monthsNumber = monthsNumber;
+        monthsNumber = 1;
+    }
+
+    public months() {
+        if monthsNumber =< 0{
+            
+        }
+        
+    }
+
+    
+    
+    
+}
